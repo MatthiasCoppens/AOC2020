35c08050e04a47f450fa88fa52d10ce96c245ec0
Matthias Coppens <coppens.matthias.abc@gmail.com>
2020-12-06 15:31:51 +0100

    Cleanup

--- day06/solution.hs
+++ day06/solution.hs
@@ -3,10 +3,10 @@ import           Data.Set (Set)
 import qualified Data.Set as Set
 
 parse :: String -> [[Set Char]]
-parse = (map . map) Set.fromList . splitOn [""] . splitOn "\n"
+parse = map (map Set.fromList . lines) . splitOn "\n\n"
 
 main :: IO ()
 main = do
     input <- parse <$> readFile "input"
-    print . sum . map (Set.size . Set.unions) $ input
-    print . sum . map (Set.size . foldl1 Set.intersection) . filter (not . null) $ input
+    print . sum . map (Set.size . Set.unions             ) $ input
+    print . sum . map (Set.size . foldl1 Set.intersection) $ input
