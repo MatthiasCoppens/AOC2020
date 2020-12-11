fe77485f9e96d1124f868526835753ac95706b74
Matthias Coppens <coppens.matthias.abc@gmail.com>
2020-12-11 12:45:51 +0100

    Day 11

---
+++ day11/solution.hs
@@ -0,0 +1,57 @@
+import           Data.Map.Strict   (Map, (!?))
+import qualified Data.Map.Strict as Map
+
+data Seat = Floor | Empty | Occupied deriving (Eq, Show)
+
+coords :: [[(Int, Int)]]
+coords = zipWith zip <$> repeat <*> map repeat $ [0,1..]
+
+parse :: String -> Map (Int, Int) Seat
+parse = Map.fromList . concat . zipWith zip coords . (map . map) tr . lines
+    where
+        tr '.' = Floor
+        tr 'L' = Empty
+        tr '#' = Occupied
+
+next1, next2 :: Map (Int, Int) Seat -> Map (Int, Int) Seat
+
+next1 grid = Map.mapWithKey f grid
+    where
+        neighbours (x, y) = length [() | n <- [(x-1, y-1), (x-1, y),
+            (x-1, y+1), (x, y-1), (x, y+1), (x+1, y-1), (x+1, y), (x+1, y+1)],
+            grid !? n == Just Occupied]
+        f _ Floor = Floor
+        f c Empty    | neighbours c == 0 = Occupied
+        f c Occupied | neighbours c <  4 = Occupied
+        f _ _ = Empty
+
+next2 grid = Map.mapWithKey f grid
+    where
+        n (x, y) (dx, dy) = case grid !? (x+dx, y+dy) of
+            Just Floor    -> n (x+dx, y+dy) (dx, dy)
+            Just Occupied -> True
+            _             -> False
+        neighbours c = length [() | dc <- [(-1,-1), (-1,0), (-1,1), (0,-1),
+            (0,1), (1,-1), (1,0), (1,1)], n c dc]
+        f _ Floor = Floor
+        f c Empty    | neighbours c == 0 = Occupied
+        f c Occupied | neighbours c <  5 = Occupied
+        f _ _ = Empty
+
+occupied :: Map a Seat -> Int
+occupied = Map.size . Map.filter (== Occupied)
+
+run :: Eq a => (a -> a) -> (a -> a)
+run f x =
+    let y = f x
+    in  if x == y then x else run f y
+
+solve1, solve2 :: Map (Int, Int) Seat -> Int
+solve1 = occupied . run next1
+solve2 = occupied . run next2
+
+main :: IO ()
+main = do
+    input <- parse <$> readFile "input"
+    print . solve1 $ input
+    print . solve2 $ input
