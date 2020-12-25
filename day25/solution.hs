0b628bd8c659a395e54f74676c42f5d666be5046
Matthias Coppens <coppens.matthias.abc@gmail.com>
2020-12-25 13:43:00 +0100

    Day 25

---
+++ day25/solution.hs
@@ -0,0 +1,17 @@
+import Data.List (findIndices)
+
+solve :: Int -> Int -> Int
+solve n1 n2 =
+    let (l, n) = find [n1, n2] $ keys 7
+    in  keys n !! l
+    where
+        keys n = iterate ((`mod` 20201227) . (* n)) 1 
+        find ms = go
+            where
+                go (n:ns) | n `elem` ms = (0, head (filter (/= n) ms))
+                          | otherwise   = let (l, n') = go ns in (succ l, n')
+
+main :: IO ()
+main = do
+    (n1:n2:_) <- map read . lines <$> readFile "input"
+    print $ solve n1 n2
